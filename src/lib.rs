d470d6b0662b1f4d12e6313080600002b2c71b5a
Janosch Gräf <janosch.graef@gmail.com>
2024-02-15 23:58:39 +0100

    wasm support

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,6 @@
 //! [2]: https://docs.rs/reqwest/latest/reqwest/index.html
 
 use std::{
-    ops::Deref,
     pin::Pin,
     task::{
         Context,
@@ -49,26 +48,16 @@ use std::{
     },
 };
 
-use async_tungstenite::WebSocketStream;
 use futures_util::{
     Sink,
     SinkExt,
     Stream,
     StreamExt,
 };
-use reqwest::{
-    header,
-    RequestBuilder,
-    Response,
-    StatusCode,
-    Upgraded,
-};
-use tokio_util::compat::{
-    Compat,
-    TokioAsyncReadCompatExt,
-};
-use tungstenite::protocol::Role;
-pub use tungstenite::Message;
+use reqwest::RequestBuilder;
+
+#[cfg(target_arch = "wasm32")]
+mod wasm;
 
 /// Errors returned by `reqwest_websocket`
 #[derive(Debug, thiserror::Error)]
@@ -79,8 +68,42 @@ pub enum Error {
     #[error("reqwest error")]
     Reqwest(#[from] reqwest::Error),
 
+    #[cfg(not(target_arch = "wasm32"))]
     #[error("tungstenite error")]
     Tungstenite(#[from] tungstenite::Error),
+
+    #[cfg(target_arch = "wasm32")]
+    #[error("web_sys error")]
+    WebSys(#[from] wasm::WebSysError),
+}
+
+#[derive(Clone, Debug)]
+pub enum Message {
+    Text(String),
+    Binary(Vec<u8>),
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl TryFrom<tungstenite::Message> for Message {
+    type Error = ();
+
+    fn try_from(value: tungstenite::Message) -> Result<Self, Self::Error> {
+        match value {
+            tungstenite::Message::Text(text) => Ok(Self::Text(text)),
+            tungstenite::Message::Binary(data) => Ok(Self::Binary(data)),
+            _ => Err(()),
+        }
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl From<Message> for tungstenite::Message {
+    fn from(value: Message) -> Self {
+        match value {
+            Message::Text(text) => Self::Text(text),
+            Message::Binary(data) => Self::Binary(data),
+        }
+    }
 }
 
 /// Trait that extends [`reqwest::RequestBuilder`] with an `upgrade` method.
@@ -102,28 +125,48 @@ impl RequestBuilderExt for RequestBuilder {
 /// websocket handshake when sent.
 pub struct UpgradedRequestBuilder {
     inner: RequestBuilder,
+
+    #[cfg(not(target_arch = "wasm32"))]
     nonce: String,
+
+    protocols: Vec<String>,
 }
 
 impl UpgradedRequestBuilder {
-    fn new(inner: RequestBuilder) -> Self {
-        let nonce = tungstenite::handshake::client::generate_key();
+    pub(crate) fn new(inner: RequestBuilder) -> Self {
+        #[cfg(not(target_arch = "wasm32"))]
+        let (nonce, inner) = {
+            let nonce = tungstenite::handshake::client::generate_key();
+            let inner = inner
+                .header(reqwest::header::CONNECTION, "upgrade")
+                .header(reqwest::header::UPGRADE, "websocket")
+                .header(reqwest::header::SEC_WEBSOCKET_KEY, &nonce)
+                .header(reqwest::header::SEC_WEBSOCKET_VERSION, "13"); // ??
 
-        let inner = inner
-            .header(header::CONNECTION, "upgrade")
-            .header(header::UPGRADE, "websocket")
-            .header(header::SEC_WEBSOCKET_KEY, &nonce)
-            .header(header::SEC_WEBSOCKET_VERSION, "13"); // ??
+            (nonce, inner)
+        };
 
-        Self { inner, nonce }
+        Self {
+            inner,
+            #[cfg(not(target_arch = "wasm32"))]
+            nonce,
+            protocols: vec![],
+        }
     }
 
     /// Sends the request and returns and [`UpgradeResponse`].
     pub async fn send(self) -> Result<UpgradeResponse, Error> {
+        #[cfg(not(target_arch = "wasm32"))]
         let inner = self.inner.send().await?;
+
+        #[cfg(target_arch = "wasm32")]
+        let inner = wasm::WebSysWebSocketStream::new(self.inner.build()?, &self.protocols).await?;
+
         Ok(UpgradeResponse {
             inner,
+            #[cfg(not(target_arch = "wasm32"))]
             nonce: self.nonce,
+            protocols: self.protocols,
         })
     }
 }
@@ -133,12 +176,22 @@ impl UpgradedRequestBuilder {
 /// This implements `Deref<Target = Response>`, so you can access all the usual
 /// information from the [`Response`].
 pub struct UpgradeResponse {
-    inner: Response,
+    #[cfg(not(target_arch = "wasm32"))]
+    inner: reqwest::Response,
+
+    #[cfg(target_arch = "wasm32")]
+    inner: wasm::WebSysWebSocketStream,
+
+    #[cfg(not(target_arch = "wasm32"))]
     nonce: String,
+
+    #[allow(dead_code)]
+    protocols: Vec<String>,
 }
 
-impl Deref for UpgradeResponse {
-    type Target = Response;
+#[cfg(not(target_arch = "wasm32"))]
+impl std::ops::Deref for UpgradeResponse {
+    type Target = reqwest::Response;
 
     fn deref(&self) -> &Self::Target {
         &self.inner
@@ -149,54 +202,88 @@ impl UpgradeResponse {
     /// Turns the response into a websocket. This checks if the websocket
     /// handshake was successful.
     pub async fn into_websocket(self) -> Result<WebSocket, Error> {
-        let headers = self.inner.headers();
+        #[cfg(not(target_arch = "wasm32"))]
+        let (inner, protocol) = {
+            let headers = self.inner.headers();
 
-        if self.inner.status() != StatusCode::SWITCHING_PROTOCOLS {
-            tracing::debug!(status_code = %self.inner.status(), "server responded with unexpected status code");
-            return Err(Error::HandshakeFailed);
-        }
+            if self.inner.status() != reqwest::StatusCode::SWITCHING_PROTOCOLS {
+                tracing::debug!(status_code = %self.inner.status(), "server responded with unexpected status code");
+                return Err(Error::HandshakeFailed);
+            }
 
-        if !headers
-            .get(header::CONNECTION)
-            .and_then(|v| v.to_str().ok())
-            .map(|s| s.eq_ignore_ascii_case("upgrade"))
-            .unwrap_or_default()
-        {
-            tracing::debug!("server responded with invalid Connection header");
-            return Err(Error::HandshakeFailed);
-        }
+            if !headers
+                .get(reqwest::header::CONNECTION)
+                .and_then(|v| v.to_str().ok())
+                .map(|s| s.eq_ignore_ascii_case("upgrade"))
+                .unwrap_or_default()
+            {
+                tracing::debug!("server responded with invalid Connection header");
+                return Err(Error::HandshakeFailed);
+            }
 
-        if !headers
-            .get(header::UPGRADE)
-            .and_then(|v| v.to_str().ok())
-            .map(|s| s.eq_ignore_ascii_case("websocket"))
-            .unwrap_or_default()
-        {
-            tracing::debug!("server responded with invalid Upgrade header");
-            return Err(Error::HandshakeFailed);
-        }
+            if !headers
+                .get(reqwest::header::UPGRADE)
+                .and_then(|v| v.to_str().ok())
+                .map(|s| s.eq_ignore_ascii_case("websocket"))
+                .unwrap_or_default()
+            {
+                tracing::debug!("server responded with invalid Upgrade header");
+                return Err(Error::HandshakeFailed);
+            }
 
-        let accept = headers
-            .get(header::SEC_WEBSOCKET_ACCEPT)
-            .and_then(|v| v.to_str().ok())
-            .ok_or(Error::HandshakeFailed)?;
-        let expected_accept = tungstenite::handshake::derive_accept_key(self.nonce.as_bytes());
-        if accept != expected_accept {
-            tracing::debug!(got=?accept, expected=expected_accept, "server responded with invalid accept token");
-            return Err(Error::HandshakeFailed);
-        }
+            let accept = headers
+                .get(reqwest::header::SEC_WEBSOCKET_ACCEPT)
+                .and_then(|v| v.to_str().ok())
+                .ok_or(Error::HandshakeFailed)?;
+            let expected_accept = tungstenite::handshake::derive_accept_key(self.nonce.as_bytes());
+            if accept != expected_accept {
+                tracing::debug!(got=?accept, expected=expected_accept, "server responded with invalid accept token");
+                return Err(Error::HandshakeFailed);
+            }
 
-        let protocol = headers
-            .get(header::SEC_WEBSOCKET_PROTOCOL)
-            .and_then(|v| v.to_str().ok())
-            .map(|s| s.to_owned());
+            let protocol = headers
+                .get(reqwest::header::SEC_WEBSOCKET_PROTOCOL)
+                .and_then(|v| v.to_str().ok())
+                .map(|s| s.to_owned());
 
-        let inner = WebSocketStream::from_raw_socket(
-            self.inner.upgrade().await?.compat(),
-            Role::Client,
-            None,
-        )
-        .await;
+            match (self.protocols.is_empty(), &protocol) {
+                (true, None) => {
+                    // we didn't request any protocols, so we don't expect one
+                    // in return
+                }
+                (false, None) => {
+                    // server didn't reply with a protocol
+                    return Err(Error::HandshakeFailed);
+                }
+                (false, Some(protocol)) => {
+                    if !self.protocols.contains(protocol) {
+                        // the responded protocol is none which we requested
+                        return Err(Error::HandshakeFailed);
+                    }
+                }
+                (true, Some(_)) => {
+                    // we didn't request any protocols but got one anyway
+                    return Err(Error::HandshakeFailed);
+                }
+            }
+
+            use tokio_util::compat::TokioAsyncReadCompatExt;
+
+            let inner = async_tungstenite::WebSocketStream::from_raw_socket(
+                self.inner.upgrade().await?.compat(),
+                tungstenite::protocol::Role::Client,
+                None,
+            )
+            .await;
+
+            (inner, protocol)
+        };
+
+        #[cfg(target_arch = "wasm32")]
+        let (inner, protocol) = {
+            let protocol = self.inner.protocol();
+            (self.inner, Some(protocol))
+        };
 
         Ok(WebSocket { inner, protocol })
     }
@@ -204,7 +291,12 @@ impl UpgradeResponse {
 
 /// A websocket connection
 pub struct WebSocket {
-    inner: WebSocketStream<Compat<Upgraded>>,
+    #[cfg(not(target_arch = "wasm32"))]
+    inner: async_tungstenite::WebSocketStream<tokio_util::compat::Compat<reqwest::Upgraded>>,
+
+    #[cfg(target_arch = "wasm32")]
+    inner: wasm::WebSysWebSocketStream,
+
     protocol: Option<String>,
 }
 
@@ -218,7 +310,19 @@ impl Stream for WebSocket {
     type Item = Result<Message, Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.inner.poll_next_unpin(cx).map_err(Into::into)
+        loop {
+            match self.inner.poll_next_unpin(cx) {
+                Poll::Pending => return Poll::Pending,
+                Poll::Ready(None) => return Poll::Ready(None),
+                Poll::Ready(Some(Err(error))) => return Poll::Ready(Some(Err(error.into()))),
+                Poll::Ready(Some(Ok(message))) => {
+                    match message.try_into() {
+                        Ok(message) => return Poll::Ready(Some(Ok(message))),
+                        Err(_) => {}
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -230,7 +334,7 @@ impl Sink<Message> for WebSocket {
     }
 
     fn start_send(mut self: Pin<&mut Self>, item: Message) -> Result<(), Self::Error> {
-        self.inner.start_send_unpin(item).map_err(Into::into)
+        self.inner.start_send_unpin(item.into()).map_err(Into::into)
     }
 
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
@@ -249,9 +353,11 @@ mod tests {
         TryStreamExt,
     };
     use reqwest::Client;
-    use tungstenite::Message;
 
-    use crate::RequestBuilderExt;
+    use super::{
+        Message,
+        RequestBuilderExt,
+    };
 
     #[tokio::test]
     async fn test_handshake() {
---
+++ src/wasm.rs
@@ -0,0 +1,259 @@
+use std::{
+    pin::Pin,
+    task::{
+        Context,
+        Poll,
+    },
+};
+
+use futures_util::{
+    Sink,
+    Stream,
+};
+use reqwest::{
+    Request,
+    Url,
+};
+use tokio::sync::{
+    mpsc,
+    oneshot,
+};
+use web_sys::{
+    js_sys::{
+        Array,
+        ArrayBuffer,
+        JsString,
+        Uint8Array,
+    },
+    wasm_bindgen::{
+        closure::Closure,
+        JsCast,
+        JsValue,
+    },
+    CloseEvent,
+    ErrorEvent,
+    Event,
+    MessageEvent,
+};
+
+use crate::Message;
+
+#[derive(Debug, thiserror::Error)]
+pub enum WebSysError {
+    #[error("invalid url: {0}")]
+    InvalidUrl(Url),
+
+    #[error("connection failed")]
+    ConnectionFailed,
+
+    #[error("{0}")]
+    ErrorEvent(String),
+
+    #[error("unknown error")]
+    Unknown,
+}
+
+impl From<ErrorEvent> for WebSysError {
+    fn from(event: ErrorEvent) -> Self {
+        Self::ErrorEvent(event.message())
+    }
+}
+
+impl From<JsValue> for WebSysError {
+    fn from(_value: JsValue) -> Self {
+        Self::Unknown
+    }
+}
+
+pub struct WebSysWebSocketStream {
+    inner: web_sys::WebSocket,
+
+    rx: mpsc::UnboundedReceiver<Option<Result<Message, WebSysError>>>,
+
+    #[allow(dead_code)]
+    on_message_callback: Closure<dyn FnMut(MessageEvent)>,
+
+    #[allow(dead_code)]
+    on_error_callback: Closure<dyn FnMut(ErrorEvent)>,
+
+    #[allow(dead_code)]
+    on_close_callback: Closure<dyn FnMut(CloseEvent)>,
+}
+
+impl WebSysWebSocketStream {
+    pub async fn new(request: Request, protocols: &Vec<String>) -> Result<Self, WebSysError> {
+        let mut url = request.url().clone();
+        let scheme = match url.scheme() {
+            "http" | "ws" => "ws",
+            "https" | "wss" => "wss",
+            _ => return Err(WebSysError::InvalidUrl(url)),
+        };
+        if let Err(_) = url.set_scheme(scheme) {
+            return Err(WebSysError::InvalidUrl(url));
+        }
+
+        // the channel for messages and errors
+        let (tx, rx) = mpsc::unbounded_channel();
+
+        // channel to signal when the websocket has been opened
+        let (open_success_tx, open_success_rx) = oneshot::channel();
+        let mut open_success_tx = Some(open_success_tx);
+
+        // channel to signal an error while opening the channel
+        let (open_error_tx, open_error_rx) = oneshot::channel();
+        let mut open_error_tx = Some(open_error_tx);
+
+        // create websocket
+        let inner = web_sys::WebSocket::new_with_str_sequence(
+            &url.to_string(),
+            &protocols
+                .into_iter()
+                .map(|s| JsString::from(s.to_owned()))
+                .collect::<Array>(),
+        )
+        .map_err(|_| WebSysError::ConnectionFailed)?;
+
+        inner.set_binary_type(web_sys::BinaryType::Arraybuffer);
+
+        // register message handler
+        let on_message_callback = {
+            let tx = tx.clone();
+            Closure::<dyn FnMut(MessageEvent)>::new(move |event: MessageEvent| {
+                tracing::debug!(event = ?event.data(), "message event");
+
+                if let Ok(abuf) = event.data().dyn_into::<ArrayBuffer>() {
+                    let array = Uint8Array::new(&abuf);
+                    let data = array.to_vec();
+                    let _ = tx.send(Some(Ok(Message::Binary(data))));
+                }
+                else if let Ok(text) = event.data().dyn_into::<JsString>() {
+                    let _ = tx.send(Some(Ok(Message::Text(text.into()))));
+                }
+                else {
+                    tracing::debug!(event = ?event.data(), "received unknown message event");
+                }
+            })
+        };
+        inner.set_onmessage(Some(on_message_callback.as_ref().unchecked_ref()));
+
+        // register error handler
+        // this will try to put the first error into a oneshot channel for errors that
+        // happen during opening. once that has been used, or the oneshot
+        // channel is dropped, this uses the regular message channel
+        let on_error_callback = {
+            let tx = tx.clone();
+            Closure::<dyn FnMut(ErrorEvent)>::new(move |event: ErrorEvent| {
+                let error = event.into();
+                tracing::debug!("received error event: {error}");
+
+                let error = if let Some(open_error_tx) = open_error_tx.take() {
+                    match open_error_tx.send(error) {
+                        Ok(()) => return,
+                        Err(error) => error,
+                    }
+                }
+                else {
+                    error
+                };
+
+                let _ = tx.send(Some(Err(error)));
+            })
+        };
+        inner.set_onerror(Some(on_error_callback.as_ref().unchecked_ref()));
+
+        // register close handler
+        let on_close_callback = {
+            let tx = tx.clone();
+            Closure::<dyn FnMut(CloseEvent)>::new(move |_event: CloseEvent| {
+                tracing::debug!("received close event");
+
+                let _ = tx.send(None);
+            })
+        };
+        inner.set_onclose(Some(on_close_callback.as_ref().unchecked_ref()));
+
+        // register open handler
+        let on_open_callback = Closure::<dyn FnMut(Event)>::new(move |_event: Event| {
+            tracing::debug!("received open event");
+            if let Some(tx) = open_success_tx.take() {
+                let _ = tx.send(());
+            }
+        });
+        inner.set_onopen(Some(on_open_callback.as_ref().unchecked_ref()));
+
+        // wait for either the open event or an error
+        tokio::select! {
+            Ok(()) = open_success_rx => {},
+            Ok(error) = open_error_rx => {
+                // cleanup
+                let _result = inner.close();
+                inner.set_onopen(None);
+                inner.set_onmessage(None);
+                inner.set_onclose(None);
+                inner.set_onerror(None);
+                return Err(error);
+            },
+            else => {
+                tracing::warn!("open sender dropped");
+            }
+        };
+
+        // remove open handler
+        inner.set_onopen(None);
+
+        Ok(Self {
+            inner,
+            on_message_callback,
+            on_error_callback,
+            on_close_callback,
+            rx,
+        })
+    }
+
+    pub fn protocol(&self) -> String {
+        self.inner.protocol()
+    }
+}
+
+impl Drop for WebSysWebSocketStream {
+    fn drop(&mut self) {
+        tracing::debug!("websocket stream dropped");
+        let _result = self.inner.close();
+        self.inner.set_onmessage(None);
+        self.inner.set_onclose(None);
+        self.inner.set_onerror(None);
+    }
+}
+
+impl Stream for WebSysWebSocketStream {
+    type Item = Result<Message, WebSysError>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.rx
+            .poll_recv(cx)
+            .map(|ready_value| ready_value.flatten())
+    }
+}
+
+impl Sink<Message> for WebSysWebSocketStream {
+    type Error = WebSysError;
+
+    fn poll_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: Message) -> Result<(), Self::Error> {
+        match item {
+            Message::Text(text) => self.inner.send_with_str(&text).map_err(Into::into),
+            Message::Binary(data) => self.inner.send_with_u8_array(&data).map_err(Into::into),
+        }
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(self.inner.close().map_err(Into::into))
+    }
+}
